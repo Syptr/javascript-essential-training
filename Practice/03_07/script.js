84521fd6867fcca77df413cbb61cf8ee31697b9c
syptr <scj.cr8s@gmail.com>
2023-09-08 11:36:30 -0400

    "creates cup object"

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,17 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const ceramicCup={
+    color: "metallic blue",
+    features: "ridges",
+    shape:"round",
+    height: 2,
+    pen: {
+        type:"marker",
+        inkColor: "black"
+    }
+}
+
+console.log("This is a ceramicCup:", ceramicCup)
+console.log("This is the ceramicCups color:", ceramicCup.color)
